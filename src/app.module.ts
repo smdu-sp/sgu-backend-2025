cb10365d48aa82688e81b2cc1b0b4d6364fa5c38
fernando <anjosdelacerda@gmail.com>
2025-05-12 15:54:21 -0300

    fix/corrigindo bug de style para gerar várias folhas em folha-setor, add lib server-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,17 @@ import { FuncionariosModule } from './funcionarios/funcionarios.module';
 import { FolhaModule } from './folha/folha.module';
 import { UnidadesModule } from './unidades/unidades.module';
 import { FeriadosModule } from './feriados/feriados.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Global()
 @Module({
   exports: [AppService],
-  imports: [PrismaModule, AuthModule, UsuariosModule, FuncionariosModule, FolhaModule, UnidadesModule, FeriadosModule],
+  imports: [PrismaModule, AuthModule, UsuariosModule, FuncionariosModule, FolhaModule, UnidadesModule, FeriadosModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'), 
+    serveRoot: '/'
+  }),
+],
   providers: [AppService,
     {
       provide: APP_GUARD,
